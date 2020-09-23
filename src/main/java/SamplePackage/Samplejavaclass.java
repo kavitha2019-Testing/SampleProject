5121c85b77a2e956e5a5ddf63622c9252a8e8ae7
kavitha2019-Testing <57364540+kavitha2019-Testing@users.noreply.github.com>
2020-09-23 10:36:38 +0100

    Update Samplejavaclass.java

--- src/main/java/SamplePackage/Samplejavaclass.java
+++ src/main/java/SamplePackage/Samplejavaclass.java
@@ -3,6 +3,7 @@ package SamplePackage;
 public class Samplejavaclass {
     public  static void main (String[] args){
         System.out.println("Sample java program");
+        System.out.println("changes thru github for Sample java program");
     }
 
 }
